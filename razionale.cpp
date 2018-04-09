6777f3a1e99cff77a745945946a9cc1e502c530e
trickpony69 <michele.clerici94@gmail.com>
2018-04-10 00:01:45 +0200

    sistemato razionale

--- razionale.cpp
+++ razionale.cpp
@@ -1,6 +1,46 @@
 #include "razionale.h"
 
-razionale::razionale(QObject *parent) : QObject(parent)
-{
+razionale::razionale(int n, int d): num(n), den(d) {
+  if(den==0) {num=0; den=1;}
+}
 
+razionale razionale::inverso() const {
+  return razionale(den,num);
+}
+
+razionale::operator double() const {
+  return static_cast<double>(num)/
+    static_cast<double>(den);
+}
+
+razionale razionale::operator+(const razionale& r) const {
+  return razionale(num*r.den + den*r.num, den*r.den);
+}
+
+razionale razionale::operator*(const razionale& r) const {
+  return razionale(num*r.num, den*r.den);
+}
+
+razionale& razionale::operator++() {
+  num += den;
+  return *this;
+}
+
+razionale razionale::operator++(int) {
+  razionale x(*this);
+  num += den;
+  return x;
+}
+
+bool razionale::operator==(const razionale& r) const {
+  return num*r.den ==r.num*den;
+}
+
+double razionale::converti()const {
+    return (this)->operator double();
+}
+
+std::ostream& operator<<(std::ostream& os, const razionale& r) {
+  //return os << "Il razionale rappresentato come un double e'" << r.operator double();
+    return os<<r.num<<"/"<<r.den<<std::endl;
 }
--- razionale.h
+++ razionale.h
@@ -1,16 +1,27 @@
 #ifndef RAZIONALE_H
 #define RAZIONALE_H
 
+#include<iostream>
 
-class razionale : public QObject
-{
-    Q_OBJECT
+class razionale;
+
+std::ostream& operator<<(std::ostream&, const razionale&);
+
+class razionale {
+    friend std::ostream& operator<<(std::ostream&, const razionale&);
+private:
+    int num, den; //den != 0
+    //meglio rappresentazioni non minimali di un razionale
 public:
-    explicit razionale(QObject *parent = nullptr);
-
-signals:
-
-public slots:
+    razionale(int =0, int =1); // agisce anche da convertitore int => Raz
+    razionale inverso() const;//gira la frazione
+    operator double() const; //cast operatore double
+    razionale operator+(const razionale&) const;
+    razionale operator*(const razionale&) const;
+    razionale& operator++();// incremento prefisso
+    razionale operator++(int);// incremento postfisso
+    bool operator==(const razionale&) const;
+    double converti() const;
 };
 
-#endif // RAZIONALE_H
+#endif
