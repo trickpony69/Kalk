2f3967143fef742f81817350fbc35e3bf3518281
marfavar <marfavar@dip171.studenti.math.unipd.it>
2018-06-19 14:59:57 +0200

    FINITO (ANCA A TESTA)

--- c++/view/wizard.cpp
+++ c++/view/wizard.cpp
@@ -6,8 +6,8 @@ wizard::wizard(QWidget *parent, Qt::WindowFlags flags): QWizard(parent,flags){
     QVBoxLayout* layoutPage0=new QVBoxLayout();
     QVBoxLayout* layoutPage1=new QVBoxLayout();
     QLabel* label0 = new QLabel("GUIDA UTILIZZO KALK \n"
-                                "\nIl corretto utilizzo della calcolatrice e' garantito se l'utente esegue la seguente scaletta di operazioni: \n1) Scrive sullo slot di inserimento. \n2) Preme 'Disegna' "
-                                "\n3) Esegue una operazione dalla barra di sinistra.\n"
+                                "\nIl corretto utilizzo della calcolatrice e' garantito se l'utente esegue la seguente scaletta di operazioni: \n1) Scrive sullo slot di inserimento. \n2) Preme 'Disegna'. "
+                                "\n3) Esegue una operazione dalla barra di sinistra.\n\n"
                                 "I bottoni in fondo alla pagina hanno le seguenti funzioni:\n"
                                 "'Aggiungi' inserisce uno slot di inserimento (max 3 slot) \n"
                                 "NB: I primi due slot sono utilizzati per le funzioni su cui sono applicati i calcoli, mentre il terzo e ultimo è sfruttato per la verifica del risultato. \n"
@@ -26,12 +26,12 @@ wizard::wizard(QWidget *parent, Qt::WindowFlags flags): QWizard(parent,flags){
     QLabel* label1 = new QLabel( "La barra sfondo nero e testo bianco contiene il risultato dell'operazione che può essere salvato tramite l'icona alla sua destra (floppy). "
                                  "L'icona a destra del floppy mostra tutti i risultati salvati, possono essere rappresentati con un semplice drag and drop su uno slot a tua scelta \n\n"
 
-                                 "L'icona alto a destra serve per accedere alle impostazioni che permettono di cambiare i colori del grafico e di modificare il range di disegno delle rette. \n\n"
-                                 "INPUT: si possono inserire rette rigorosamente in questa forma 'ax + by + c = 0' (implicita), punti nella forma (x;y) , quadrati e triangoli mettendo in sequenza i punti , es. (x1;y1)(x2;y2) ecc.. \n"
-                                 "I coefficenti delle rette e i valori dei punti possono essere interi, razionali(es. 3/4 oppure 5/8) o double (es. 3.67 e non 3,67). \n"
+                                 "L'icona in alto a destra (ingranaggio) serve per accedere alle impostazioni che permettono di cambiare i colori del grafico e di modificare il range di disegno delle rette. \n\n"
+                                 "INPUT: si possono inserire rette rigorosamente in questa forma 'ax + by + c = 0' (implicita e con '0' dopo l'uguale), punti nella forma (x;y) , quadrati e triangoli mettendo in sequenza i punti , es. (x1;y1)(x2;y2) ecc.. \n"
+                                 "I coefficenti delle rette e i valori dei punti possono essere interi, razionali(es. 3/4 oppure 5/8) o double. Utilizzare il simbolo '.' per differenziare la parte intera dalla parte decimale invece del simbolo ',' (es. 3.67 e non 3,67). \n"
 
-                                 "\nATTENZIONE: Gli slot vanno riempiti in ordine crescente. \nPurtroppo il coefficente della x o della y, nel caso razioanle, è stato considerato"
-                                 "(anche se in maniera matematicamente sbagliato) in questa maniera: 6/7x rappresenta (6/7)x. "
+                                 "\nATTENZIONE: Gli slot vanno riempiti in ordine crescente. \nPurtroppo il coefficente della x o della y, nel caso razionale, è stato considerato"
+                                 "(anche se in maniera matematicamente sbagliato) in questo modo: 6/7x rappresenta (6/7)x. "
 
                                  "\n\nESEMPIO INPUT :  \n3x+7/5y-7=0 \n5/7x-y  \n(0;9) \n(2/5;6)(5;9)(4;0) \n");
     label0->setWordWrap(true);
